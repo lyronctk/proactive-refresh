70d4bf79ef16edaaa868dd7f49203c2cfa1c98d0
Kaylee George <62825936+kayleegeorge@users.noreply.github.com>
2023-02-18 23:55:21 -0800

    Update build_dummy.sh

--- circom/scripts/build_dummy.sh
+++ circom/scripts/build_dummy.sh
@@ -2,9 +2,9 @@
 
 ### Boilerplate circuit compilation and vkey/zkey generation for development
 # Powers of tau selection for Hermez Rollup
-PTAU=../circuits/pot15_final.ptau
+PTAU=/Users/kayleegeorge/code/zk/ats-pr-bls/circom/circuits/pot15_final.ptau
 CIRCUIT_NAME=dummy
-BUILD_DIR=../build/"$CIRCUIT_NAME"
+BUILD_DIR=/Users/kayleegeorge/code/zk/ats-pr-bls/circom/build
 
 if [ -f "$PTAU" ]; then
     echo "Found Phase 1 ptau file"
@@ -16,7 +16,7 @@ fi
 # Compile circuit
 echo "****COMPILING CIRCUIT****"
 start=`date +%s`
-circom "$CIRCUIT_NAME".circom --r1cs --wasm --sym --output "$BUILD_DIR"
+circom "$CIRCUIT_NAME".circom --r1cs --wasm --sym --output "$BUILD_DIR"/
 export CPATH="$CPATH:/opt/homebrew/opt/nlohmann-json/include:/opt/homebrew/opt/gmp/include"
 end=`date +%s`
 echo "DONE COMPILING CIRCUIT ($((end-start))s)"
@@ -39,10 +39,10 @@ echo "DONE EXPORTING VERIFICATION KEY ($((end-start))s)"
 # yarn run snarkjs zkey verify verif-manager.r1cs $PTAU verif-manager.zkey
 
 # Generate the witness, primarily as a smoke test for the circuit
-node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/"$CIRCUIT_NAME".wasm "$BUILD_DIR"/"$CIRCUIT_NAME".json "$BUILD_DIR"/"$CIRCUIT_NAME".wtns
+node "$BUILD_DIR"/"$CIRCUIT_NAME"_js/generate_witness.js "$BUILD_DIR"/dummy_js/"$CIRCUIT_NAME".wasm "$BUILD_DIR"/"$CIRCUIT_NAME".json "$BUILD_DIR"/"$CIRCUIT_NAME".wtns
 
 # Export verifier to smart contract for on-chain verification
-yarn run snarkjs zkey export solidityverifier "$CIRCUIT_NAME".zkey "$CIRCUIT_NAME"Verifier.sol
+yarn run snarkjs zkey export solidityverifier "$BUILD_DIR"/"$CIRCUIT_NAME".zkey "$BUILD_DIR"/Verifier.sol
 sed -i -e 's/0.6.11;/0.8.13;/g' "$CIRCUIT_NAME"Verifier.sol
 mv "$CIRCUIT_NAME"Verifier.sol ../contracts
 rm "$CIRCUIT_NAME"Verifier.sol-e
